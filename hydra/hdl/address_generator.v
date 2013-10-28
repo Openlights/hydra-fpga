94cf530ca588aa0cc5b62d8f39e77b5cc4622e0a
Jon Evans <jon@craftyjon.com>
2013-10-28 18:56:12 -0400

    Working testbench for address generator

--- hydra/hdl/address_generator.v
+++ hydra/hdl/address_generator.v
@@ -12,28 +12,39 @@
  module address_generator (
     clk,
     rst_n,
-
+    strand_offset,
+    strand_idx,
+    strand_length,
     addr
     );
 
     parameter MEM_ADDR_WIDTH = 24;
+    parameter STRAND_PARAM_WIDTH = 16;
 
     input clk;
     input rst_n;
+
+    // The active strand will drive these three buses
+    input [STRAND_PARAM_WIDTH-1:0] strand_offset;
+    input [STRAND_PARAM_WIDTH-1:0] strand_idx;
+    input [STRAND_PARAM_WIDTH-1:0] strand_length;
 
     output reg [MEM_ADDR_WIDTH-1:0] addr;
 
     // Address generation process
     always @(posedge clk) begin
         if (rst_n == 1'b0) begin
-            // initialize stuff
             addr = { MEM_ADDR_WIDTH {1'b0} };
         end
         else begin
-            // normal operation
-            
             // address of memory is given by 
-            // (strand_offset[current] + strand_idx[current]) % strand_length[current]
+            // strand_offset[current] + (strand_idx[current] % strand_length[current])
+            if (strand_idx > strand_length - 1) begin
+                addr = strand_offset + (strand_idx - strand_length);
+            end
+            else begin
+                addr = strand_offset + strand_idx;
+            end
         end
     end
 
--- hydra/testbench/address_generator_tb.v
+++ hydra/testbench/address_generator_tb.v
@@ -12,9 +12,14 @@
 module address_generator_tb;
 
     parameter MEM_ADDR_WIDTH = 24;
+    parameter STRAND_PARAM_WIDTH = 16;
 
     reg clk = 0;
     reg rst_n = 1;
+
+    reg [STRAND_PARAM_WIDTH-1:0] strand_offset = 0;
+    reg [STRAND_PARAM_WIDTH-1:0] strand_idx = 0;
+    reg [STRAND_PARAM_WIDTH-1:0] strand_length = 100;
 
     wire [MEM_ADDR_WIDTH-1:0] addr;
 
@@ -22,6 +27,9 @@ module address_generator_tb;
     address_generator #(MEM_ADDR_WIDTH) ag_inst (
             .clk(clk),
             .rst_n(rst_n),
+            .strand_offset(strand_offset),
+            .strand_idx(strand_idx),
+            .strand_length(strand_length),
             .addr(addr)
         );
 
@@ -36,6 +44,17 @@ module address_generator_tb;
 
         #10 rst_n = 0;
         #10 rst_n = 1;
+        #10 strand_offset = 320;
+        strand_idx = 1;
+        #20 strand_idx = 50;
+        #20 strand_idx = 99;
+        #20 strand_idx = 100;
+        #20 strand_idx = 101;
+        #20 strand_offset = 640;
+        strand_idx = 1;
+        #20 strand_idx = 50;
+        #20 strand_idx = 100;
+        #20 strand_idx = 101;
 
         #500 $finish;
     end
